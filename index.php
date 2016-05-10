dd73aab50239f8615d64684b09ae698e8b0a0772
Oscar Ruiz <oscar.tahuilan@televisatim.com>
2016-05-10 00:10:30 -0500

    WD: update index/css

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<meta content="Escuela Wood And Design" name="author" />
 	<meta content="Escuela Wood And Design y Escuela Preparatoria" name="description" />
 	<meta content="Escuela, Especialidad, Carpintería, Profesionalismo, Preparatoria, Prepa, Diseño, Madera, Industria Madedera, Especialistas, Juventud, Oportunidad" name="keywords" />
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1" />
 </head>
 <body>
 	<div class="wd-stage">
